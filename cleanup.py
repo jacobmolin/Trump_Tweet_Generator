1e69c153e5eaa36764d8b73a290e9e42d440c5ad
jacobmolin <jacob.molle.molin@gmail.com>
2020-12-07 19:24:43 +0100

    ready for training with characters

--- cleanup.py
+++ cleanup.py
@@ -14,9 +14,12 @@ import re
 # print(len(df))
 
 
-def cleanup(df):
+def cleanup_text(text):
     # Remove all rows that are retweets
-    df = df[~(df.isRetweet == "t")]
+    # df = df[~(df.isRetweet == "t")]
+
+    # Remove all rows that are answers to tweets
+    # df = df[~(df.text.str.contains('"""'))]
 
     # Delete re-tweets
     # df = df[~df.text.str.contains("RT")]
@@ -27,24 +30,28 @@ def cleanup(df):
     # print(type(df.iloc[0].isRetweet))
 
     # All tweets in one string
-    text = ''.join(df["text"])
+    # text = ''.join(df["text"])
 
     # Remove all links on tweets
-    text = re.sub(r'http\S+', ' ', text)
+    # text = re.sub(r'http\S+', ' ', text)
 
     # Put space before @
-    text = text.replace('@', ' @')
+    # text = text.replace('@', ' @')
 
     # Replace &amp
-    text = text.replace('&amp', '&')
+    # text = text.replace('&amp', '&')
 
     # Replace #
-    text = text.replace('#', ' #')
+    # text = text.replace('#', ' #')
 
     replacement = [',', '.', '!', '\"', ':',
                    '?', '(', ')', '’', '“', '‘', '~', '-', '—', '/', '”', '–']
+    ['\"', '“', '~', '-', '\\', '/', '”']
     for r in replacement:
         text = text.replace(r, ' ')
+
+    # Remove multiple whitespaces
+    # text = ' '.join(text.split())
 
     # Lower capital letters to get fewer unique words
     text = text.lower()
@@ -53,3 +60,101 @@ def cleanup(df):
     return text
     # print(len(text.split(' ')))
     # print(text.split(' '))
+
+
+def cleanup_for_char(text):
+    # Remove all rows that are retweets
+    # df = df[~(df.isRetweet == "t")]
+
+    # Remove all rows that are answers to tweets
+    # df = df[~(df.text.str.contains('"""'))]
+
+    # Delete re-tweets
+    # df = df[~df.text.str.contains("RT")]
+    # print('df = df[~df.text.str.contains("RT")]')
+    # print(df.dtypes)
+
+    # print(df.iloc[0].isRetweet)
+    # print(type(df.iloc[0].isRetweet))
+
+    # All tweets in one string
+    # text = ''.join(df["text"])
+
+    # Remove all links on tweets
+    # text = re.sub(r'http\S+', ' ', text)
+
+    # Put space before @
+    # text = text.replace('@', ' @')
+
+    # Replace &amp
+    # text = text.replace('&amp', '&')
+
+    # Replace #
+    # text = text.replace('#', ' #')
+
+    # replacement = [',', '.', '!', '\"', ':',
+    #                '?', '(', ')', '’', '“', '‘', '~', '-', '—', '/', '”', '–']
+    # ['\"', '“', '~', '-', '\\', '/', '”']
+    replacement = ['-', '(1/2)', '(2/2)']
+    for r in replacement:
+        text = text.replace(r, ' ')
+
+    # Remove multiple whitespaces
+    text = ' '.join(text.split())
+
+    # Lower capital letters to get fewer unique words
+    text = text.lower()
+
+    # print(text)
+    return text
+    # print(len(text.split(' ')))
+    # print(text.split(' '))
+
+
+def cleanup_tweets(df):
+
+    # Remove all rows that are retweets
+    df = df[~(df.isRetweet == "t")]
+
+    # Remove all rows that are answers to tweets
+    df = df[~(df.text.str.contains('"""'))]
+
+    text_list = df.text.tolist()
+
+    for i in range(len(text_list)):
+        if '".' in text_list[i]:
+            text_list[i] = text_list[i].replace('".', '')
+            text_list[i] = text_list[i][0:len(text_list[i])-1]
+            # print(text_list[i][len(text_list[i])-1])
+
+        # Remove all links on tweets
+        text_list[i] = re.sub(r'http\S+', ' ', text_list[i])
+
+        # Put space before @
+        text_list[i] = text_list[i].replace('.@', ' @')
+
+        # Put space before @
+        text_list[i] = text_list[i].replace('@', ' @')
+
+        # Replace &amp
+        text_list[i] = text_list[i].replace('&amp,', '&')
+
+        # Replace #
+        text_list[i] = text_list[i].replace('#', ' #')
+
+        # Replace ""
+        text_list[i] = text_list[i].replace('""', '"')
+
+        # Replace .
+        # text_list[i] = text_list[i].replace('.', '. ')
+
+        # replacement = ['\"', '“', '~', '-', '\\', '/', '”']  # '’', '‘',
+        # for r in replacement:
+        #     text_list[i] = text_list[i].replace(r, ' ')
+
+        text_list[i] = ' '.join(text_list[i].split())
+
+        # Lower capital letters to get fewer unique words
+        # text_list[i] = text_list[i].lower()
+
+    return text_list
--- modelling.py
+++ modelling.py
@@ -1,25 +1,48 @@
-
+from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.utils import np_utils
 
 
-def modelling(model, X_modified, Y_modified):
-    model.add(LSTM(700, input_shape=(
+def modelling(X_modified, Y_modified, run_model_fit):
+    model = Sequential()
+
+    layers = 3
+    units = 700
+    drop = 0.2
+    batch_size = 100
+    epochs = 20
+
+    model.add(LSTM(units, input_shape=(
         X_modified.shape[1], X_modified.shape[2]), return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(700, return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(700))
-    model.add(Dropout(0.2))
+    model.add(Dropout(drop))
+    model.add(LSTM(units, return_sequences=True))
+    model.add(Dropout(drop))
+    model.add(LSTM(units))
+    model.add(Dropout(drop))
     model.add(Dense(Y_modified.shape[1], activation='softmax'))
+
+    # model.add(LSTM(400, input_shape=(
+    #     X_modified.shape[1], X_modified.shape[2]), return_sequences=True))
+    # model.add(Dropout(drop))
+    # model.add(LSTM(400))
+    # model.add(Dropout(drop))
+    # model.add(Dense(Y_modified.shape[1], activation='softmax'))
 
     model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-    model.fit(X_modified, Y_modified, epochs=3, batch_size=100)
+    filename = ''
 
-    model.save_weights(
-        'models/text_generator_gigant_700_0.2_700_0.2_700_0.2_20.h5')
+    if run_model_fit:
+        # Create filename
+        filename = 'models/text_generator'
+        for i in range(layers):  # Write layer info
+            filename += f'_{units}_{drop}'
+        filename += f'_e{epochs}_bs{batch_size}.h5'
 
-    return model
+        # model.fit(X_modified, Y_modified, epochs=epochs, batch_size=batch_size)
+        print('filename:', filename)
+        model.save_weights(filename)
+
+    return [model, filename]
